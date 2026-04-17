97c3705b374c6e56be00d29b9260ee1b68478ba1
Zaven525 <71457724+Zaven525@users.noreply.github.com>
2026-04-17 16:30:29 +0400

    Add files via upload

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <vector>
 
-
-void quickSort(std::vector<int>& nums, size_t l, size_t r)
+int particion(std::vector<int>& nums, int l, int r)
 {
-    if (l >= r) return;
-    int pivot = r;
     int i = l - 1;
     int j = l;
     for (; j <= r; ++j)
     {
-        if (nums[j] <= nums[pivot])
+        if (nums[j] <= nums[r])
         {
             std::swap(nums[++i], nums[j]);
         }
     }
+    return i;
+}
+
+void quickSort(std::vector<int>& nums, int l, int r)
+{
+    if (l >= r) return;
+    int pivot = particion(nums, l, r);
+    
     quickSort(nums, l, pivot - 1);
     quickSort(nums, pivot + 1, r);
 }
@@ -22,7 +27,7 @@ void quickSort(std::vector<int>& nums, size_t l, size_t r)
 
 int main()
 {
-    std::vector<int> nums{3, 5, 2, 6, 4};
+    std::vector<int> nums{};
     quickSort(nums, 0, nums.size() - 1);
 
     for (int elem : nums) std::cout << elem << ", ";
